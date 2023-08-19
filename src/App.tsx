2286d7e4be9b62d2519b445a45c33659f15cb1a2
luanrem <luanrem@gmail.com>
2023-08-19 10:51:56 -0300

    feat: add a box

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Mesh } from 'three'
 import './App.css'
 
+function Box() {
+  const boxRef = useRef<Mesh>(null!);
+
+  useFrame(() => {
+    boxRef.current.rotation.x += 0.005;
+    boxRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={boxRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
+function ThreeScene() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[5, 5, 5]} />
+      <Box />
+    </Canvas>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App h-screen">
+        <ThreeScene />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
